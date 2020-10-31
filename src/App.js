397343e71631d3d317e22e6aed231663df14f8a3
Gautam Arora <gautamarora6248@gmail.com>
2020-10-31 09:09:46 +0530

    Added images

--- src/App.js
+++ src/App.js
@@ -5,6 +5,31 @@ import NavComponent from "./components/NavComponent";
 import HomeComponent from "./components/HomeComponent";
 
 function App() {
+  function Doctor() {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify({ _id: "5f9c3ae6b6a05e2c29dc3cfa" });
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      "http://88a58535d3d0.ngrok.io/api/doctor/getdoctordetails",
+      requestOptions
+    )
+      .then((response) => response.json())
+      // .then((response) => this.setState.doctor(patientDetails:))
+      .then((result) => {
+        this.setState({ doctor: result.doctor });
+      })
+      .catch((error) => console.log("error", error));
+  }
+
   return (
     <>
       <Router>
@@ -17,7 +42,9 @@ function App() {
             <Switch>
               <Route exact path="/Home" component={HomeComponent} />
 
-              <Route exact path="/Patients" component={PatientComponent} />
+              <Route exact path="/Patients">
+                <PatientComponent doctorData={Doctor()} />
+              </Route>
             </Switch>
           </div>
         </div>
